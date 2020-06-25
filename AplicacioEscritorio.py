4899a525bfb3db4b16d99ae6bafabef674339e30
MateoBres <55922654+MateoBres@users.noreply.github.com>
2020-06-25 12:47:15 -0300

    Update AplicacioEscritorio.py

--- AplicacioEscritorio.py
+++ AplicacioEscritorio.py
@@ -1,69 +1,89 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
+import sqlite3
 
-alumnos = {}
+conn = sqlite3.connect("pileta.db")
 
+cursor = conn.cursor()
 
-def ver_lista():
-    print("Lista de alumnos:")
-    # El bucle "for" aplicado sobre un diccionario recorre sus
-    # claves.
-    for nombre in alumnos:
-        cursos = alumnos[nombre]
-        print(nombre + " - " + str(cursos) + " cursos")
+try:
+	cursor.execute('CREATE TABLE alumnos (nombre TEXT, cursos NUMERIC);')
+except sqlite3.OperationalError:
+	pass
 
 
 def agregar():
-    # Obtener el nombre de la caja de texto.
     nombre_alumno = caja_nombre.get()
-    # Obtener la cantidad de cursos de la caja de texto y convertirla
-    # a un entero.
-    cursos = int(caja_cursos.get())
+    cursos = lista_despegable.get()
+    alumno = (nombre_alumno, cursos)
+    alumno = list(alumno)
     if nombre_alumno == "":
-        print("Error: no ha ingresado un nombre válido.")
+        messagebox.showerror(message="Error: no ha ingresado un nombre válido.", title="Sistema")
     else:
-        # Agregar un nuevo par clave-valor al diccionario "alumnos".
-        # La clave es el nombre del alumno y el valor, la cantidad
-        # de cursos.
-        alumnos[nombre_alumno] = cursos
-        print("Has ingresado el alumno correctamente.")
-
-
+        lista1.insert(0, str(alumno[0]) +'  ' + str(alumno[1]))
+        conn = sqlite3.connect("pileta.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO alumnos VALUES(?, ?);", (nombre_alumno, cursos))
+        conn.commit()
+        messagebox.showinfo(message="Has ingresado el alumno correctamente.", title="Sistema")
+        
 def ver_cursos():
-    # Obtener el nombre de la caja de texto.
-    nombre = caja_nombre.get()
-    print("Cursos: " + str(alumnos[nombre]))
+	nombre = caja_nombre.get()
+	for alumno in alumnos:
+			if  nombre == alumno[0]:
+				messagebox.showinfo(message="Cursos: " + str(alumno[1]), title="Sistema")
+				break
+			else:
+				messagebox.showwarning(message="El alumno no esta' presente en la lista.", title="Sistema")
 
+def nuevo():
+	print('Nuevo archivo.')
+	
+def acerca_de():
+	print('Acerca de:')
 
 ventana = tk.Tk()
-ventana.config(width=600, height=400)
-#ventana.state('zoomed')
-ventana.title("Proyecto integrador")
 
-boton_lista = tk.Button(text="Ver lista de alumnos", command=ver_lista)
-boton_lista.place(x=10, y=10)
+ventana.title("La pileta de Liliana")
 
-etiqueta_nombre = tk.Label(text="Nombre alumno:")
-etiqueta_nombre.place(x=10, y=60)
+menu_barra = tk.Menu()
 
-caja_nombre = tk.Entry()
-caja_nombre.place(x=110, y=60, width=150, height=20)
+menu_archivo = tk.Menu(menu_barra, tearoff = 0)
+menu_archivo.add_command(label = 'Nuevo', command = nuevo)
 
-etiqueta_cursos = tk.Label(text="Cursos:")
-etiqueta_cursos.place(x=10, y=100)
+menu_ayuda = tk.Menu(menu_barra, tearoff = 0)
+menu_ayuda.add_command(label = 'Acerca de....', command = acerca_de)
 
-caja_cursos = tk.Entry()
-caja_cursos.place(x=110, y=100, width=50, height=20)
+menu_barra.add_cascade(label = 'Archivo', menu = menu_archivo)
+menu_barra.add_cascade(label = 'Ayuda', menu = menu_ayuda)
 
-boton_agregar = tk.Button(text="Agregar a la lista", command=agregar)
-boton_agregar.place(x=10, y=150)
+etiqueta_nombre = ttk.Label(text="Nombre alumno:")
+etiqueta_nombre.place(x=10, y=20)
 
-boton_cursos = tk.Button(text="Ver cantidad de cursos", command=ver_cursos)
-boton_cursos.place(x=115, y=150)
+caja_nombre = ttk.Entry()
+caja_nombre.place(x=110, y=20, width=150, height=20)
+
+etiqueta_cursos = ttk.Label(text="Cursos:")
+etiqueta_cursos.place(x=10, y=50)
+
+lista_despegable = ttk.Combobox(values = [1, 2, 3, 4])
+lista_despegable.place(x=110, y=50, width=50, height=20)
+
+boton_agregar = ttk.Button(text="Agregar a la lista", command=agregar)
+boton_agregar.place(x=10, y=100)
+
+boton_cursos = ttk.Button(text="Ver cantidad de cursos", command=ver_cursos)
+boton_cursos.place(x=115, y=100)
 
 lista1 = tk.Listbox()
-lista1.place(x=10, y=200, width=300, height=250)
+lista1.place(x=10, y=130, width=300, height=250)
+cursor.execute("SELECT * FROM alumnos;")
+alumnos = cursor.fetchall()
+for alumno in alumnos:
+	lista1.insert(tk.END, alumno[0] + '  ' + str(alumno[1]))
+ventana.config(width=600, height=400, menu = menu_barra)
 
+conn.close()
 
 ventana.mainloop()
